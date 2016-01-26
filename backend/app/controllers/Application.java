54d4de8426a3e3d69a4e263f3f8df55ac8599997
Adithya <aprabhakaran@scu.edu>
2016-01-26 00:54:25 -0800

    Testing testing testing

--- backend/app/controllers/Application.java
+++ backend/app/controllers/Application.java
@@ -8,7 +8,10 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render("Lucem is ready."));
     }
 
+    public static Result test(){
+	return ok(index.render("Test successful."));
+    }
 }
